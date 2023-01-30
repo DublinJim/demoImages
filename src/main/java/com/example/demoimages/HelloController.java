0f769ad67de23a867b64a9aeb8a499ca3a38ebb8
DublinJim <59180380+DublinJim@users.noreply.github.com>
2023-01-30 10:35:12 -0500

    image loads not as resource

--- src/main/java/com/example/demoimages/HelloController.java
+++ src/main/java/com/example/demoimages/HelloController.java
@@ -1,7 +1,26 @@
 package com.example.demoimages;
 
-public class HelloController {
+import javafx.fxml.Initializable;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class HelloController implements Initializable {
 
 
+    public ImageView imageView;
+    public Image image;
+    public AnchorPane rootPane;
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        image = new Image("testCard.png");
+        imageView = new ImageView(image);
+        imageView.fitHeightProperty().bind(rootPane.heightProperty());
+        imageView.fitWidthProperty().bind(rootPane.widthProperty());
+        rootPane.getChildren().add(imageView);
+    }
 }
