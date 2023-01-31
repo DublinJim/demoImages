e16d30e920ec8b92d5a4b2984d92621154107862
DublinJim <59180380+DublinJim@users.noreply.github.com>
2023-01-31 15:20:14 -0500

    array added

--- src/main/java/com/example/demoimages/GetAllFonts.java
+++ src/main/java/com/example/demoimages/GetAllFonts.java
@@ -1,16 +1,26 @@
 package com.example.demoimages;
-import java.awt.GraphicsEnvironment;
+
+import java.awt.*;
+import java.util.ArrayList;
+
 public class GetAllFonts {
+    private ArrayList<String> fontArray = new ArrayList<>();
+
+
 
     public void getList() {
         String[] fonts = GraphicsEnvironment.getLocalGraphicsEnvironment().getAvailableFontFamilyNames();
         for (String font : fonts) {
             System.out.println(font);
-
+            fontArray.add(font);
         }
         int noFonts = fonts.length;
         System.out.println("---------------------------------------");
-        System.out.println("Total of "+noFonts+" fonts available,");
+        System.out.println("| Total of " + noFonts + " fonts available        |");
         System.out.println("---------------------------------------");
     }
+    public ArrayList<String> getFontArray() {
+        return fontArray;
+    }
+
 }
--- src/main/java/com/example/demoimages/HelloController.java
+++ src/main/java/com/example/demoimages/HelloController.java
@@ -3,6 +3,7 @@ package com.example.demoimages;
 import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
@@ -16,6 +17,7 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 public class HelloController implements Initializable {
+    private ComboBox<String> comboBox = new ComboBox<>();
     private final String clickSound = "src/main/resources/FreeOfficeSFX_MouseSingleClick.wav";
     public ImageView imageView;
     public Image image;
@@ -56,7 +58,6 @@ public class HelloController implements Initializable {
         Image imageBin = new Image(imageName);
 
         btn1.setOnAction(actionEvent -> {
-
 
             imageView.setImage(imageBin);
             playClick(clickSound);
